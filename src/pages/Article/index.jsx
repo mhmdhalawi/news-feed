e00a8a4200211273353ee1d9c46aab08768abbf1
mhmdhalawi <mohammadhalawi93@gmail.com>
2021-11-14 01:40:01 +0200

    fix article fetching error

--- src/pages/Article/index.jsx
+++ src/pages/Article/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { useStore } from '../../store';
 import imageDefault from '../../assets/ArticleDefault.jpg';
@@ -6,27 +6,35 @@ import imageDefault from '../../assets/ArticleDefault.jpg';
 import { image_url } from '../../constants';
 
 export default function index() {
+  const [headline, setHeadline] = useState('');
+  const [paragraph, setParagraph] = useState('');
+  const [catergory, setCatergory] = useState('');
+  const [image, setImage] = useState('');
+
   const { id } = useParams();
-  const setFilterArticles = useStore((state) => state.setFilterArticles);
-  const filterArticle = useStore((state) => state.filteredArticle);
+  const setFilteredArticles = useStore((state) => state.setFilterArticles);
+  const filteredArticle = useStore((state) => state.filteredArticle);
+
   useEffect(() => {
-    setFilterArticles(id);
+    setFilteredArticles(id);
   }, []);
 
-  const {
-    headline: { main },
-    lead_paragraph,
-    type_of_material,
-    multimedia,
-  } = filterArticle[0];
-  const image = multimedia[1].url ? image_url + multimedia[1].url : imageDefault;
+  useEffect(() => {
+    if (filteredArticle.length === 1) {
+      const { headline, type_of_material, lead_paragraph, multimedia } = filteredArticle[0];
+      setHeadline(headline.main);
+      setParagraph(lead_paragraph || '');
+      setCatergory(type_of_material || '');
+      setImage(multimedia.length > 1 ? image_url + multimedia[1].url : imageDefault);
+    }
+  }, [filteredArticle]);
 
   return (
     <div className='w-1/2 mx-auto'>
       <img src={image} />
-      <h1>{main}</h1>
-      <h1>{type_of_material}</h1>
-      <p>{lead_paragraph}</p>
+      <h1>{headline}</h1>
+      <h1>{catergory}</h1>
+      <p>{paragraph}</p>
     </div>
   );
 }
